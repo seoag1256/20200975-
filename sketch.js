6a59f01500c47f14dbb63617fe85ebf0e7b52de2
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-10-21 03:42:16 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,18 +11,27 @@ function draw() {
   
   noStroke();
   
-  fill(243, 210, 193, 255);
+  fill(243, 210, 193);
   triangle(547, 255, 610, 273, 568, 316);
+  
+  fill(195, 170, 151);
+  triangle(571, 345, 568, 315, 617, 335);
+  
+  fill(231, 185, 159);
+  triangle(610, 273, 568, 315, 617, 335);
+  
+  fill(226, 201, 194);
+  triangle(610, 273, 617, 335, 675, 310);
+  
+  fill(225, 193, 170);
+  triangle(674, 310, 644, 361, 708, 335);
 
-  
-  
-  
-  if (mouseIsPressed) {
+}
+
+function mousePressed() {
     // 좌표 출력
     console.log(mouseX, mouseY);
     // 색상 출력
-    var colorSpuit = get(mouseX, mouseY);
+    let colorSpuit = get(mouseX, mouseY);
     console.log(colorSpuit);
   }
-  
-}
