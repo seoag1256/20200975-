62006b4559b5eb74fa83bacf42fa5635a31a3d04
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-10-21 02:48:51 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,5 +6,23 @@ function setup() {
 }
 
 function draw() {
+  
   image(img, 0, 0, 1280, 720);
+  
+  noStroke();
+  
+  fill(243, 210, 193, 255);
+  triangle(547, 255, 610, 273, 568, 316);
+
+  
+  
+  
+  if (mouseIsPressed) {
+    // 좌표 출력
+    console.log(mouseX, mouseY);
+    // 색상 출력
+    var colorSpuit = get(mouseX, mouseY);
+    console.log(colorSpuit);
+  }
+  
 }
